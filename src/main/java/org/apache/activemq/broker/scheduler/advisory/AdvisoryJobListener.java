e64a86fe3658ccfd9e3856d4bb263e9e24a94eb5
Max Fortun <Max.Fortun@wsj.com>
2023-06-20 12:28:07 -0400

    updated

--- src/main/java/org/apache/activemq/broker/scheduler/advisory/AdvisoryJobListener.java
+++ src/main/java/org/apache/activemq/broker/scheduler/advisory/AdvisoryJobListener.java
@@ -30,12 +30,57 @@ import org.slf4j.LoggerFactory;
 public class AdvisoryJobListener implements JobListener {
     private static final Logger LOG = LoggerFactory.getLogger(AdvisoryJobListener.class);
 
+	/**
+     * Used to specify that a some operation should be performed on the Scheduled Message,
+     * the Message must have an assigned Id for this action to be taken.
+     */
+    public static final String AMQ_SCHEDULER_ACTION = "AMQ_SCHEDULER_ACTION";
+
+    /**
+     * Indicates that a browse of the Scheduled Messages is being requested.
+     */
+    public static final String AMQ_SCHEDULER_ACTION_BROWSE = "BROWSE";
+    /**
+     * Indicates that a Scheduled Message is to be remove from the Scheduler, the Id of
+     * the scheduled message must be set as a property in order for this action to have
+     * any effect.
+     */
+    public static final String AMQ_SCHEDULER_ACTION_REMOVE = "REMOVE";
+    /**
+     * Indicates that all scheduled Messages should be removed.
+     */
+    public static final String AMQ_SCHEDULER_ACTION_REMOVEALL = "REMOVEALL";
+
+    /**
+     * A property that holds the beginning of the time interval that the specified action should
+     * be applied within.  Maps to a long value that specified time in milliseconds since UTC.
+     */
+    public static final String AMQ_SCHEDULER_ACTION_START_TIME = "ACTION_START_TIME";
+    /**
+     * A property that holds the end of the time interval that the specified action should be
+     * applied within.  Maps to a long value that specified time in milliseconds since UTC.
+     */
+    public static final String AMQ_SCHEDULER_ACTION_END_TIME = "ACTION_END_TIME";
+
 	private JobListener delegateJobListener; 
 
 	public AdvisoryJobListener(JobListener delegateJobListener) {
 		this.delegateJobListener = delegateJobListener;
 	}
 
+	public void willScheduleJob(String id, ByteSequence job) throws Exception  {
+	}
+
+    public void scheduleJob(String id, ByteSequence job) throws Exception {
+	}
+
+    public void didScheduleJob(String id, ByteSequence job) throws Exception {
+	}
+
+    public void willDispatchJob(String id, ByteSequence job) throws Exception {
+	}
+
+	// Actual dispatching of the job
 	@Override
 	public void scheduledJob(String id, ByteSequence job) {
 		if(null == delegateJobListener) {
@@ -43,4 +88,26 @@ public class AdvisoryJobListener implements JobListener {
 		}
 		delegateJobListener.scheduledJob(id, job);
 	}
+
+    public void didDispatchJob(String id, ByteSequence job) throws Exception {
+	}
+
+    public void willRemoveJob(String id) throws Exception {
+	}
+
+    public void removeJob(String id) throws Exception {
+	}
+
+    public void didRemoveJob(String id) throws Exception {
+	}
+
+    public void willRemoveRange(long start, long end) throws Exception {
+	}
+
+    public void removeRange(long start, long end) throws Exception {
+	}
+
+    public void didRemoveRange(long start, long end) throws Exception {
+	}
 }
+
